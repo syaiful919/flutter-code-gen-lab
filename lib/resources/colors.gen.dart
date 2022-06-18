a941d4eb7714bfe29286c9212184ac0108469417
Syaiful Izzuddin Salam <syaiful.izzuddin@ruangguru.com>
2022-06-18 13:19:17 +0700

    add assets, generate using fluttergen

---
+++ lib/resources/colors.gen.dart
@@ -0,0 +1,16 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// ignore_for_file: directives_ordering
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+}
